45af82e3d7437afa8fe9c6c655de8894779ea8b1
Will Mobley <feulmob+github@gmail.com>
2025-06-13 10:34:23 -0500

    changed Singularity to apptainer.
    Singularity at tacc was deprecated.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-singularity run --bind ${_tapisExecSystemInputDir}/images_dir,${_tapisExecSystemOutputDir}/code/images:/  --writable-tmpfs /corral/tacc/aci/PT2050/community/ODM/odm_latest.sif\
+apptainer run --bind ${_tapisExecSystemInputDir}/images_dir,${_tapisExecSystemOutputDir}/code/images:/  --writable-tmpfs /corral/tacc/aci/PT2050/community/ODM/odm_latest.sif\
   --orthophoto-png --mesh-octree-depth 12 --ignore-gsd --dtm --smrf-threshold 0.4\
    --smrf-window 24 --dsm --pc-csv --pc-las  --rerun-all --orthophoto-kmz --force-gps \
  --ignore-gsd  --matcher-type flann --feature-quality ultra --max-concurrency 128\
