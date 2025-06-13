4d9a75c14aa40cbe264de2690a495f744d2fc7b6
Will Mobley <feulmob+github@gmail.com>
2025-06-13 09:29:37 -0500

    Created initial commit for running singularity based ODM-cli

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM debian:bullseye-slim
 
 COPY run.sh /bin/run.sh
-
 RUN chmod +x /bin/run.sh
 
 ENTRYPOINT [ "run.sh" ]
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-Greeting=$1
-Target=$2
 
-FULL_GREETING="${Greeting} ${Target}. My name is ${_tapisJobOwner}"
-echo "$FULL_GREETING"
-
-echo $FULL_GREETING > $_tapisExecSystemOutputDir/out.txt
+singularity run --bind ${_tapisExecSystemInputDir}/images_dir,${_tapisExecSystemOutputDir}/code/images:/  --writable-tmpfs /corral/tacc/aci/PT2050/community/ODM/odm_latest.sif\
+  --orthophoto-png --mesh-octree-depth 12 --ignore-gsd --dtm --smrf-threshold 0.4\
+   --smrf-window 24 --dsm --pc-csv --pc-las  --rerun-all --orthophoto-kmz --force-gps \
+ --ignore-gsd  --matcher-type flann --feature-quality ultra --max-concurrency 128\
+  --cog --pc-classify --sky-removal  --pc-quality high --rerun-all  --use-hybrid-bundle-adjustment --build-overviews --time --min-num-features 10000\
+   --project-path ${_tapisExecSystemInputDir}/images_dir
